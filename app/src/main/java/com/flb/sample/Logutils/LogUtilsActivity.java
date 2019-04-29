ee26788be21e72bfaeea24ba48f132d07a713506
fzw <fengzhangwei@senyint.com>
2019-04-29 16:23:01 +0800

    表单

--- app/src/main/java/com/flb/sample/Logutils/LogUtilsActivity.java
+++ app/src/main/java/com/flb/sample/Logutils/LogUtilsActivity.java
@@ -5,10 +5,22 @@ import android.os.Bundle;
 
 
 import com.flb.sample.Logutils.bean.Kuaidi;
+import com.flb.sample.Logutils.http.Api;
+import com.flb.sample.Logutils.http.ApiService;
 import com.flb.sample.Logutils.http.RetrofitManager;
 import com.flb.sample.Logutils.utils.LogUtil;
 import com.flb.sample.R;
 
+import java.io.File;
+import java.util.List;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
@@ -19,28 +31,48 @@ public class LogUtilsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_utils);
-        init();
+       // init();
     }
 
     private void init() {
-        RetrofitManager.getInstance().getApiService()
-                .query("a3d3a43fcc149b6ed8268b8fa41d27b7", "json", "遗落的世界")
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Kuaidi>() {
-                    @Override
-                    public void onCompleted() {
-                    }
 
-                    @Override
-                    public void onError(Throwable e) {
-                        LogUtil.e("请求失败", e);
-                    }
 
-                    @Override
-                    public void onNext(Kuaidi kuaidi) {
-                        LogUtil.d("请求成功");
-                    }
-                });
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.baseUrl)
+                .build();
+        ApiService service = retrofit.create(ApiService.class);
+        //1.创建MultipartBody.Builder对象
+        MultipartBody.Builder builder = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM);//表单类型
+
+//2.获取图片，创建请求体
+        File file=new File("路径");
+        RequestBody body = RequestBody.create(MediaType.parse("multipart/form-data"),file);//表单类型
+
+//3.调用MultipartBody.Builder的addFormDataPart()方法添加表单数据
+        builder.addFormDataPart("key", "华为云上传对象的唯一标识");//传入服务器需要的key，和相应value值
+        builder.addFormDataPart("AccessKeyId", "接口返回的参数accessKeyId");
+        builder.addFormDataPart("policy", "接口返回的参数policy");
+        builder.addFormDataPart("signature", "接口返回的参数signature");
+        builder.addFormDataPart("file",file.getName(),body); //添加数据，body创建的请求体
+
+//4.创建List<MultipartBody.Part> 集合，
+//  调用MultipartBody.Builder的build()方法会返回一个新创建的MultipartBody
+//  再调用MultipartBody的parts()方法返回MultipartBody.Part集合
+        List<MultipartBody.Part> parts=builder.build().parts();
+
+//5.最后进行HTTP请求，传入parts即可
+        Call<Kuaidi> call = service.myUpload(parts);
+        call.enqueue(new Callback<Kuaidi>() {
+            @Override
+            public void onResponse(Call<Kuaidi> call, Response<Kuaidi> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Kuaidi> call, Throwable t) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/flb/sample/Logutils/http/ApiService.java
+++ app/src/main/java/com/flb/sample/Logutils/http/ApiService.java
@@ -1,9 +1,15 @@
 package com.flb.sample.Logutils.http;
 import com.flb.sample.Logutils.bean.Kuaidi;
 
+import java.util.List;
+
+import okhttp3.MultipartBody;
+import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import rx.Observable;
 
 public interface ApiService {
@@ -12,5 +18,9 @@ public interface ApiService {
     @POST(Api.PartUrl.QUERY)
     Observable<Kuaidi> query(@Field("key") String key, @Field("dtype") String dType, @Field("q") String name);
 
+
+    @Multipart                          //这里用Multipart
+    @POST(Api.PartUrl.QUERY)                //请求方法为POST，里面为你要上传的url
+    Call<Kuaidi> myUpload(@Part List<MultipartBody.Part> partLis);
 
 }
